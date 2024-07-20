faedd8ac0d8c5b2fd68de65bd6e87ca36c6685c4
zprl <zprl.dev@gmail.com>
2024-07-20 12:17:38 +0900

    up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu};
 
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  let system_tray = SystemTray::new()
+    .with_menu(SystemTrayMenu::new().add_item(CustomMenuItem::new("test1", "title")));
+
+  tauri::Builder::default()
+    .system_tray(system_tray)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
