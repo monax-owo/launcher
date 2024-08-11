8478da32d4260d821cdc9691d14435e8c7b9088d
zprl <zprl.dev@gmail.com>
2024-08-11 23:03:44 +0900

    fix

--- src/lib/components/Template.svelte
+++ src/lib/components/Template.svelte
@@ -2,9 +2,8 @@
   export let test = "1536px";
 </script>
 
-<!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="Template" style:--test={test} on:click|stopPropagation={() => {}}>
+<div class="Template" role="button" tabindex="0" style:--test={test} on:click|stopPropagation>
   <slot><div>no content</div></slot>
 </div>
 
